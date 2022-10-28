753094e567531a47aa4e5aae55e860ad234eda66
servosdesign <servos.design0@gmail.com>
2022-10-28 16:07:21 -0500

    animations, animation speed, playmovement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-  private Rigidbody2D playerRB;
+  private Rigidbody2D playerRigidbody;
+  private Animator playerMovementAnim;
 
   [SerializeField]
   private float movementSpeed;
+  [SerializeField]
+  private float animationSpeed;
 
   void Start() {
-    playerRB = GetComponent<Rigidbody2D>();
+    playerRigidbody = GetComponent<Rigidbody2D>();
+    playerMovementAnim = GetComponent<Animator>();
   }
 
   void Update() {
-    playerRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * movementSpeed * Time.deltaTime;
+    playerMovementAnim.SetFloat("moveX", playerRigidbody.velocity.x);
+    playerMovementAnim.SetFloat("moveY", playerRigidbody.velocity.y);
 
+
+    playerMovementAnim.speed = animationSpeed;
+  }
+
+  void FixedUpdate() {
+    playerRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * movementSpeed * Time.deltaTime;
   }
 }
