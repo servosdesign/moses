f5a2c33ec3ffd43a46e272190d6652428772f0aa
servosdesign <servos.design0@gmail.com>
2022-11-07 18:02:19 -0600

    main menu buttons

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+  public void Scene1() {
+    SceneManager.LoadScene("Scene1");
+  }
+
+  public void Scene2() {
+    SceneManager.LoadScene("Scene2");
+  }
+  
+  public void QuitGame() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    Application.Quit();
+  }
+}
