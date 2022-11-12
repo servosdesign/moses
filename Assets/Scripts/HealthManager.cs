84c1e4e5461311de848e1b0b04cc42dca513c4d1
servosdesign <servos.design0@gmail.com>
2022-11-12 17:48:22 -0600

    flash on damage animation

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -4,22 +4,67 @@ using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
+  private SpriteRenderer playerSprite;
+
   public int currentHealth;
   public int maxHealth;
+  private bool flashActive;
+  [SerializeField]
+  private float flashLength = 0f;
+  private float flashCounter = 0f;
 
   void Start()
   {
-
+    playerSprite = GetComponent<SpriteRenderer>();
   }
 
   void Update()
   {
-
+    if (flashActive)
+    {
+      if (flashCounter > flashLength * 0.99f)
+      {
+        playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+      }
+      else if (flashCounter > flashLength * 0.82f)
+      {
+        playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+      }
+      else if (flashCounter > flashLength * 0.66f)
+      {
+        playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+      }
+      else if (flashCounter > flashLength * 0.49f)
+      {
+        playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+      }
+      else if (flashCounter > flashLength * 0.33f)
+      {
+        playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+      }
+      else if (flashCounter > flashLength * 0.16f)
+      {
+        playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+      }
+      else if (flashCounter > 0f)
+      {
+        playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
+      }
+      else
+      {
+        playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+        flashActive = false;
+      }
+      flashCounter -= Time.deltaTime;
+    }
   }
 
   public void HurtPlayer(int damageToGive)
   {
     currentHealth -= damageToGive;
+    flashActive = true;
+    flashCounter = flashLength;
+
     if (currentHealth <= 0)
     {
       gameObject.SetActive(false);
